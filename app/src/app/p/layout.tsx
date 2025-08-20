47f0d1ae2d35f7324d6df3d05eca2e34dd2c7808
Subham Mani <manisubham09@gmail.com>
2025-08-20 20:44:12 +0530

    update

--- app/src/app/p/layout.tsx
+++ app/src/app/p/layout.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from "react";
 
 const PatientLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="bg-gradient-to-b from-blue-700 via-neutral-200 to-white">
+    <div className="bg-gradient-to-b from-emerald-700 via-neutral-200 to-white">
       {children}
     </div>
   );
