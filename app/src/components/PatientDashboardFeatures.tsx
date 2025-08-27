f5dfaeb2396b17242f3e23193c4007db098f1727
Subham Mani <manisubham09@gmail.com>
2025-08-27 19:26:56 +0530

    update

--- app/src/components/PatientDashboardFeatures.tsx
+++ app/src/components/PatientDashboardFeatures.tsx
@@ -8,9 +8,12 @@ import {
   Calendar,
   FlaskConical,
   Camera,
-  HeartPulse
+  HeartPulse,
 } from "lucide-react";
 import DashboardCard from "./DashboardCard";
+import { EventsList } from "./EventsList";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const features = [
   {
@@ -34,10 +37,23 @@ const features = [
     icon: <HeartPulse className="w-8 h-8 text-emerald-400" />,
     link: "/p/coach",
   },
-
 ];
 
 const PatientDashboardFeatures = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    async function fetchEvents() {
+      try {
+        const response = await axios.get("/api/list-event/all");
+        setEvents(response.data);
+      } catch (error) {
+        console.error("Error fetching events:", error);
+      }
+    }
+    fetchEvents();
+  }, []);
+
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
@@ -52,7 +68,16 @@ const PatientDashboardFeatures = () => {
           />
         ))}
       </div>
-      
+      {events.length > 0 ? (
+        <div>
+          <h2 className="text-2xl text-white font-bold mt-12 mb-6">
+            Upcoming Health Events
+          </h2>
+          <EventsList events={events} />
+        </div>
+      ) : (
+        <p className="text-base mt-12">No upcoming events.</p>
+      )}
     </div>
   );
 };
